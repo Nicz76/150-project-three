9f136fc08e747a1493492e90bf245c9f433c2145
mango-cat <98450325+mango-cat@users.noreply.github.com>
2023-05-30 16:13:55 -0700

    Wrote tests for phase 1 & phase2 + added to makefile

---
+++ apps/test_phase2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <fs.h>
+
+#define ASSERT(cond, func)                               \
+do {                                                     \
+	if (!(cond)) {                                       \
+		fprintf(stderr, "Function '%s' failed\n", func); \
+		exit(EXIT_FAILURE);                              \
+	}                                                    \
+} while (0)
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	char *diskname;
+	int fd;
+	char data[26];
+
+	if (argc < 1) {
+		printf("Usage: %s <diskimage>\n", argv[0]);
+		exit(1);
+	}
+
+	/* Mount disk */
+	diskname = argv[1];
+	ret = fs_mount(diskname);
+	ASSERT(!ret, "fs_mount");
+
+    /* Print info about disk */
+    ret = fs_info();
+    ASSERT(!ret, "fs_info");
+    printf("FIXME: reminder - fs_info incomplete\n");
+
+    /* Create file */
+    ret = fs_create("myfile.txt");
+    ASSERT(!ret, "fs_create");
+
+    /* Check info after file creation */
+    ret = fs_info();
+    ASSERT(!ret, "fs_info");
+
+    /* List files on disk - should see myfile.txt */
+    ret = fs_ls();
+    ASSERT(!ret, "fs_ls");
+
+    /* Delete file */
+    ret = fs_delete("myfile.txt");
+    ASSERT(!ret, "fs_delete");
+
+    /* Check info after file deletion */
+    ret = fs_info();
+    ASSERT(!ret, "fs_info");
+
+    /* List files on disk - should see nothing */
+    ret = fs_ls();
+    ASSERT(!ret, "fs_ls");
+
+    /* Unmount disk */
+	ret = fs_umount();
+    ASSERT(!ret, "fs_umount");
+
+	return 0;
+}
