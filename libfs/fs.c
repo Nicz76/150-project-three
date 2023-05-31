85a6ec6e62825fc96b79d38da3474a45b1d3bbbc
Nicz76 <niczop2167@gmail.com>
2023-05-30 19:09:36 -0700

    Improve Phase 1 and 2 code

--- libfs/fs.c
+++ libfs/fs.c
@@ -7,10 +7,11 @@
 #include "disk.h"
 #include "fs.h"
 
-#define FAT_EOC NULL
+#define FAT_EOC 0xFFFF
 
 struct __attribute__((__packed__)) superblock {
-	uint64_t signature;
+	// uint64_t signature;
+	uint8_t signature[8];
 	uint16_t total_blocks;
 	uint16_t root_dir_idx;
 	uint16_t data_blks_idx;
@@ -56,41 +57,56 @@ struct filedes FDT[FS_OPEN_MAX_COUNT];
 
 int fs_mount(const char *diskname)
 {	
-	uint64_t expected_signature = 0x4543533135304653;  // "ECS150FS"
+	// uint64_t expected_signature = 0x4543533135304653;  // "ECS150FS"
 	
 	// Error checking: Virtual file disk @diskname cannot be opened
 	if (block_disk_open(diskname) == -1) {
+		perror("Block disk open");
 		return -1;
 	}
 
 	// Read superblock from virtual disk
 	block_read(0, &sb);  // FIXME: correct usage?
 
+	// printf("Read superblock\n");
+	// printf("Signature: %lX\n", sb.signature);
+
 	// Error checking: file system has expected format
-	if (sb.signature != expected_signature) {
+	if (memcmp(sb.signature, "ECS150FS", 8)) {
+		perror("Wrong signature");
 		return -1;
 	}
+
+	// printf("Superblock signature matches\n");
 
     // Error checking: total number of blocks is same as block_disk_count() of total
 	int total_number_of_block = 1 + (int)(sb.FAT_blocks)+ 1 + (int)(sb.total_data_blks);
 	if (total_number_of_block != block_disk_count()) {
-		printf("Number of blocks do not match\n");
+		perror("Number of blocks do not match\n");
 		return -1;
 	}
 
+	// printf("Number of blocks matches block_disk_count()\n");
+
     // Allocate FAT blocks
     // FAT = malloc((int)sb.FAT_blocks * sizeof(struct FAT_block));
-	int entries_per_FAT_block = BLOCK_SIZE / sizeof(uint16_t);
+	int entries_per_FAT_block = BLOCK_SIZE / 2;
 	FAT = malloc((int)sb.FAT_blocks * entries_per_FAT_block);
+
+	// printf("Allocated FAT blocks\n");
 
     // Read FAT from virtual disk
 	for (int i = 0; i < (int)sb.FAT_blocks; i++) {
-		block_read(i + 1, &FAT[i * entries_per_FAT_block]);
+		block_read(i + 1, &FAT[(i * entries_per_FAT_block)]);
 	}
+
+	// printf("Read FAT from disk\n");
 
 	// Read root directory from virtual disk
 	block_read(sb.root_dir_idx, &root_dir);
 	
+	// printf("Read root directory from disk\n");
+		
 	// Initialize rdir_free_entries: number of free entries in the root directory
 	rdir_free_entries = 0;
 	// Count and update number of free entries
@@ -99,6 +115,8 @@ int fs_mount(const char *diskname)
 			rdir_free_entries++;
 	}
 
+	// printf("Initialized root directory entries\n");
+	
 	// Initialize file descriptor table (FDT)
 	// set each filedes to "empty" by initializing first character of filename to null char
 	for (int i = 0; i < FS_OPEN_MAX_COUNT; i++) {
@@ -106,7 +124,7 @@ int fs_mount(const char *diskname)
 		FDT[i].offset = 0;
 	}
 
-	printf("Created virtual disk '%s' with '%d' data blocks\n", "diskname", (int)(sb.total_data_blks ));
+	// printf("Created virtual disk '%s' with '%d' data blocks\n", "diskname", (int)(sb.total_data_blks ));
 	//find the diskname (checked)
 	//Open the virtual disk (Checked)
 	//Read the metadata(superbloc, fat, root directory) (Unsured)
@@ -116,13 +134,18 @@ int fs_mount(const char *diskname)
 }
 
 int fs_umount(void)
-{
-	if (block_disk_close() == -1){
-		//Checks if the virtual disk file is open
+{	//Check if there are open file descriptors
+ 	if (total_file_open != 0){
+		printf("There are still open file descriptors\n");
+		return -1;
+	}
+
+	//Checks if the virtual disk file is open
+	if (block_disk_close() == -1){		
 		return -1;
 	} 
 
-	free(FAT);
+	// free(FAT);
 	return 0;
 	//Close virtual disk(make sure the virtual disk is up to date)
 	//* Return: -1 if no FS is currently mounted, or if the virtual disk cannot be
@@ -141,6 +164,14 @@ int fs_info(void)
 
 //For FAT ratio by Professor
 //for loop to loop all the entries of the FAT table
+	int fat_free = 0;
+	int total_fat_entries = sb.total_data_blks;
+
+	for (int i = 0; i < total_fat_entries; i++){
+		if (FAT[i] == 0){
+			fat_free++;
+		}
+	}
 	//Show info about volume
 	/* TODO: Phase 1 */
 	printf("FS Info:\n");
@@ -150,7 +181,7 @@ int fs_info(void)
 	printf("data_blk=%d\n", sb.data_blks_idx);
 	printf("data_blk_count=%d\n", sb.total_data_blks);
 	// FIXME:
-	// printf("fat_free_ratio=%d/%d\n", );
+	printf("fat_free_ratio=%d/%d\n", fat_free, total_fat_entries);
 	printf("rdir_free_ratio=%d/%d\n", rdir_free_entries, FS_FILE_MAX_COUNT);
 	
 	return 0;
@@ -160,7 +191,7 @@ int fs_create(const char *filename)
 {
 	// Error checking: no FS mounted or @filename is invalid or string @filename is too long
 	// or root directory already contains %FS_FILE_MAX_COUNT files (i.e. no free entries)
-	if (block_disk_count == -1 || filename == NULL || (strlen(filename) + 1) > FS_FILENAME_LEN || rdir_free_entries == 0) {
+	if (block_disk_count() == -1 || filename == NULL || (strlen(filename) + 1) > FS_FILENAME_LEN || rdir_free_entries == 0) {
 		return -1;
 	}
 
@@ -187,11 +218,11 @@ int fs_create(const char *filename)
 	// }
 	
 	// Add filename entry to root directory
-	strcpy(root_dir[first_avail_idx].filename, filename);  // FIXME: need memcpy instead?
+	memcpy(root_dir[first_avail_idx].filename, filename, strlen(filename) + 1);  
 	// root_dir[first_avail_idx].filename=(uint8_t)(filename);
 	root_dir[first_avail_idx].size = 0;
 	root_dir[first_avail_idx].data_start_idx = FAT_EOC;
-	//int *ptr = FAT.entries;
+	// root_dir[first_avail_idx].data_start_idx = 0; 
 
 	// Decrement number of free entries in root directory
 	rdir_free_entries--;
@@ -213,14 +244,14 @@ int fs_delete(const char *filename)
 {
 	/* TODO: Phase 2 */
 	// Error checking: no FS mounted or @filename is invalid
-	if (block_disk_count == -1 || filename == NULL) {
+	if (block_disk_count() == -1 || filename == NULL) {
 		return -1;
 	}
 	
 	// Find the entry corresponding to @filename in root directory
 	int file_idx = -1;
 	for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {  
-		if (!strcmp(root_dir[i].filename, filename)) {
+		if (!strcmp((char*)root_dir[i].filename, filename)) {
 			file_idx = i;
 			break;
 		}
@@ -245,7 +276,7 @@ int fs_delete(const char *filename)
 	}
 	
 	// Remove file's entry from root directory
-	root_dir[file_idx].size = NULL;
+	root_dir[file_idx].size = 0;
 	root_dir[file_idx].filename[0] = '\0';
 
 	// Increment number of free entries in root directory
@@ -257,7 +288,7 @@ int fs_delete(const char *filename)
 int fs_ls(void)
 {
 	// Error checking: no underlying virtual disk was opened
-	if (block_disk_count == -1){
+	if (block_disk_count() == -1){
 		return -1;
 	}
 
@@ -283,7 +314,7 @@ int fs_open(const char *filename)
 {
 	// Error checking: no FS mounted or @filename is invalid 
 	// or there are already %FS_OPEN_MAX_COUNT files currently open
-	if (block_disk_count == -1 || filename == NULL || total_file_open >= FS_OPEN_MAX_COUNT) {
+	if (block_disk_count() == -1 || filename == NULL || total_file_open >= FS_OPEN_MAX_COUNT) {
 		return -1;
 	}
 	
@@ -293,7 +324,7 @@ int fs_open(const char *filename)
 	// Find @filename in root_dir
 	int rdir_file_idx = -1;
 	for (int i = 0; i < FS_FILE_MAX_COUNT; i++){
-		if (root_dir[i].filename == filename) {
+		if (!strcmp((char*)root_dir[i].filename, filename)) {
 			rdir_file_idx = i;
 			break;
 		}
@@ -316,7 +347,7 @@ int fs_open(const char *filename)
 	}
 
 	// Populate the filedes struct for this file (??)
-	strcpy(FDT[fd].filename, root_dir[rdir_file_idx].filename);
+	memcpy(FDT[fd].filename, root_dir[rdir_file_idx].filename, FS_FILENAME_LEN);
 	FDT[fd].data_start_idx = root_dir[rdir_file_idx].data_start_idx;
 	FDT[fd].offset = 0;
 
@@ -350,7 +381,7 @@ int fs_open(const char *filename)
 int fs_close(int fd)
 {
 	// Error checking: no FS mounted or @fd is invalid (out of bounds or not currently open)
-	if (block_disk_count == -1 || fd < 0 || fd >= FS_OPEN_MAX_COUNT || FDT[fd].filename[0] == '\0') {
+	if (block_disk_count() == -1 || fd < 0 || fd >= FS_OPEN_MAX_COUNT || FDT[fd].filename[0] == '\0') {
 		return -1;
 	}
 
@@ -386,14 +417,14 @@ int fs_close(int fd)
 int fs_stat(int fd)
 {
 	// Error checking: no FS mounted or @fd is invalid (out of bounds or not currently open)
-	if (block_disk_count == -1 || fd < 0 || fd >= FS_OPEN_MAX_COUNT || FDT[fd].filename[0] == '\0') {
+	if (block_disk_count() == -1 || fd < 0 || fd >= FS_OPEN_MAX_COUNT || FDT[fd].filename[0] == '\0') {
 		return -1;
 	}
 
 	// Find file in root directory to grab its size
 	uint32_t file_size;
 	for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
-		if (!strcmp(FDT[fd].filename, root_dir[i].filename)) {
+		if (!strcmp((char*)FDT[fd].filename, (char*)root_dir[i].filename)) {
 			file_size = root_dir[i].size;
 			break;
 		}
@@ -415,7 +446,7 @@ int fs_lseek(int fd, size_t offset)
 {
 	// Error checking: no FS mounted or @fd is invalid (out of bounds or not currently open)
 	//                 or @offset is larger than current file size
-	if (block_disk_count == -1 || fd < 0 || fd >= FS_OPEN_MAX_COUNT || FDT[fd].filename[0] == '\0' || offset > fs_stat(fd)) {
+	if (block_disk_count() == -1 || fd < 0 || fd >= FS_OPEN_MAX_COUNT || FDT[fd].filename[0] == '\0' || offset > (size_t)fs_stat(fd)) {
 		return -1;
 	}
 
@@ -436,12 +467,37 @@ int fs_lseek(int fd, size_t offset)
 //  * invalid (i.e., out of bounds, or not currently open), or if @offset is larger
 //  * than the current file size. 0 otherwise.
 }
-
+/*
 int fs_write(int fd, void *buf, size_t count)
 {
-	/* TODO: Phase 4 */
+	// Error checking: no FS mounted or @fd is invalid (out of bounds or not currently open) or @buf is NULL
+	if (block_disk_count == -1 || fd < 0 || fd >= FS_OPEN_MAX_COUNT || FDT[fd].filename[0] == '\0' || buf == NULL) {
+		return -1;
+	}
+
+	 TODO: Phase 4 
 	//Modify and sycyhn data everytime there is
 	//a change in the data blocks
+	*
+ * fs_write - Write to a file
+ * @fd: File descriptor
+ * @buf: Data buffer to write in the file
+ * @count: Number of bytes of data to be written
+ *
+ * Attempt to write @count bytes of data from buffer pointer by @buf into the
+ * file referenced by file descriptor @fd. It is assumed that @buf holds at
+ * least @count bytes.
+ *
+ * When the function attempts to write past the end of the file, the file is
+ * automatically extended to hold the additional bytes. If the underlying disk
+ * runs out of space while performing a write operation, fs_write() should write
+ * as many bytes as possible. The number of written bytes can therefore be
+ * smaller than @count (it can even be 0 if there is no more space on disk).
+ *
+ * Return: -1 if no FS is currently mounted, or if file descriptor @fd is
+ * invalid (out of bounds or not currently open), or if @buf is NULL. Otherwise
+ * return the number of bytes actually written.
+ 
 }
 
 int fs_read(int fd, void *buf, size_t count)
@@ -602,7 +658,7 @@ int fs_read(int fd, void *buf, size_t count)
 //  * return the number of bytes actually read.
 //  */
 	/* TODO: Phase 4 */
-}
+//}
 
 
 /**
@@ -610,7 +666,7 @@ int fs_read(int fd, void *buf, size_t count)
  * @fd: File descriptor
  * 
  * Return: index of the data block corresponding to the file's offset
- */
+ *
 static int get_data_block_idx(int fd)
 {
 	// File size is 0 - does not occupy any data blocks
@@ -648,3 +704,48 @@ static int get_data_block_idx(int fd)
 
 	return idx;
 }
+
+//Helper Function for fs_write() to extend file size
+// int extend_file_size(int fd, int bytes_left)
+// {
+	
+// 	int ptr;
+// 	//get the current EOC
+// 	 ptr = FDT[fd].data_start_idx;
+// 	 while (ptr != FAT_EOC){
+// 		ptr = FAT[ptr];
+// 	 }
+
+// 	//Travse the FAT array to find space to allocate new blocks of
+// 	//datablocks
+
+// 	int end_of_idx = -1;
+// 	for (int i = 0; i <(sb.FAT_blocks * BLOCK_SIZE / 2); i++){		//Fixme: Need the size of the FAT array
+// 		if (FAT[i] == 0){
+// 			FAT[ptr] = i;
+// 			FAT[i] = FAT_EOC;
+// 			end_of_idx = i;
+// 			break;
+// 		}
+// 	}
+// 	//If there are no more available space then return	
+// 	if (end_of_idx == -1){
+// 		return -1;
+// 	}
+
+// 	//Find the size of the file
+// 	int size;
+// 	int root_dir_file;
+// 	for (int i = 0; i < FS_OPEN_MAX_COUNT; i++) {
+// 		if (!strcmp(FDT[fd].filename, root_dir[i].filename)) {
+// 			size = root_dir[i].size;
+// 			root_dir_file = i;
+// 			break;
+// 		}
+// 	}
+
+// 	//Increase current file size
+// 	size = size + BLOCK_SIZE;
+// 	root_dir[i].size = size;
+// }
+*/
